276d059eb7f97c96291dac1e06f94875aaa3819a
vunguyen011000 <vunguyen011000@gmail.com>
2024-05-16 08:40:28 +0000

    feat: init source

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+        brightness: Brightness.dark,
+        seedColor: const Color.fromARGB(255, 119, 185, 232)),
+    textTheme: GoogleFonts.lobsterTextTheme());
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+    );
+  }
+}
